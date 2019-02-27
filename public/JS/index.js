226d3efdbc198207ce4cfa32ed7da0539c7e6e94
Salman-Shaik <dukesalman5@gmail.com>
2019-02-27 17:48:15 +0530

    Add clear count feature in the UI

--- public/JS/index.js
+++ public/JS/index.js
@@ -6,50 +6,67 @@ const createTrWithData = (tag, ...args) => {
 
 const getEle = selectorInput => document.querySelector(selectorInput);
 
-const getStatus = ticks => {
+const getStatus = count => {
   const statusDot = createElement('span','','dot red');
-  if(ticks <= 4 && ticks > 2) statusDot.className = 'dot yellow'
-  if(ticks >= 5 ) statusDot.className = 'dot green'
+  if(count <= 4 && count > 2) statusDot.className = 'dot yellow'
+  if(count >= 5 ) statusDot.className = 'dot green'
   return statusDot;
 }
 
-const getTallyMarks = (tallyList,ticks) => {
-  for (let i = 0; i < ticks; i++) appendChilds(tallyList,createElement('li'));
+const getTallyMarks = (tallyList,count) => {
+  for (let i = 0; i < count; i++) appendChilds(tallyList,createElement('li'));
 }
 
-const updateTally = (tr,ticks) => {
+const updateTally = (tr,count) => {
   const tallyList = createElement('ol');
-  getTallyMarks(tallyList,ticks);
+  getTallyMarks(tallyList,count);
   const tallyData = createElement('td');
   appendChilds(tallyData,tallyList);
   appendChilds(tr, tallyData);
 }
-const updateStatus = (tr,ticks) => {
-  const statusSpan = getStatus(ticks);
+const updateStatus = (tr,count) => {
+  const statusSpan = getStatus(count);
   const spanData = createElement('td');
   appendChilds(spanData,statusSpan);
   appendChilds(tr, spanData);
 }
 
-const addTickButton = tr => {
-  const tickButton = createElement('button', 'Add', 'addTick');
+const appendAddButton = tr => {
+  const add = createElement('button', '+', 'add');
   const tickData = createElement('td');
-  appendChilds(tickData,tickButton);
+  appendChilds(tickData,add);
   appendChilds(tr,tickData);
 }
 
-const updateMemberDetails = (table, name,ticks) => {
+const appendRemoveButton = tr => {
+  const remove = createElement('button', '-', 'remove');
+  const tickData = createElement('td');
+  appendChilds(tickData,remove);
+  appendChilds(tr,tickData);
+}
+
+const updateMemberDetails = (table, name,count) => {
   let tr = createTrWithData('td',name);
-  updateTally(tr,ticks);
-  updateStatus(tr,ticks);
-  addTickButton(tr);
+  updateTally(tr,count);
+  updateStatus(tr,count);
+  appendAddButton(tr);
+  appendRemoveButton(tr);
   appendChilds(table,tr);
 }
 
-const addTick = ({target}) => {
-  const memberName = target.parentNode.parentNode.firstChild.innerText;
+const fetchMemberName = node => node.parentNode.parentNode.firstChild.innerText;
+
+const incrementCount = ({target}) => {
+  const memberName = fetchMemberName(target);
   const params = JSON.stringify({"memberName":memberName});
   sendAjaxRequest("PUT","/tick",'',params);
+  fetchMeterDetails();
+}
+
+const clearCount = ({target}) => {
+  const memberName = fetchMemberName(target)
+  const params = JSON.stringify({"memberName":memberName});
+  sendAjaxRequest("DELETE","/tick",'',params);
   fetchMeterDetails();
 }
 
@@ -58,14 +75,16 @@ const displayMemberDetails = function() {
   const shownDetails = getEle('table');
   const updatedDetails = createElement('table');
   const heading = createTrWithData(
-    'th', 'Name' , 'Number Of Ticks', 'Treat Status', 'Add Tick');
+    'th', 'Name' , 'Count', 'Treat Status', 'Add', 'Clear');
   updatedDetails.appendChild(heading);
   Object.keys(response).forEach((key,index)=>{
       updateMemberDetails(updatedDetails, key, response[key]);
   })
   shownDetails.replaceWith(updatedDetails);
-  let addButtons = document.querySelectorAll('button[class=addTick]');
-  addButtons.forEach(button => button.onclick = addTick);
+  let addButtons = document.querySelectorAll('button[class=add]');
+  let removeButtons = document.querySelectorAll('button[class=remove]');
+  addButtons.forEach(button => button.onclick = incrementCount);
+  removeButtons.forEach(button => button.onclick = clearCount);
 }
 
 const fetchMeterDetails = () => {
