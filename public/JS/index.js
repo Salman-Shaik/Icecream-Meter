116ff0024511388a7f677756be295a5368420d9b
Salman-Shaik <dukesalman5@gmail.com>
2019-02-27 09:51:17 +0530

    Displaying the member details and adding ad tick feature.

--- public/JS/index.js
+++ public/JS/index.js
@@ -10,7 +10,7 @@ const appendChilds = function(node, ...childs) {
 };
 
 const createTrWithData = function(tag, ...args) {
-  let tr = createElement('tr');
+  let tr = createElement('tr','',args[0]);
   args.forEach(arg => appendChilds(tr, createElement(tag, arg)));
   return tr;
 };
@@ -18,20 +18,53 @@ const createTrWithData = function(tag, ...args) {
 const getEle = selectorInput => document.querySelector(selectorInput);
 
 const getStatus = ticks => {
-  const statusDot = createElement('span','','dot green');
+  const statusDot = createElement('span','','dot red');
   if(ticks <= 4 && ticks > 2) statusDot.className = 'dot yellow'
-  if(ticks >= 5 ) statusDot.className = 'dot red'
+  if(ticks >= 5 ) statusDot.className = 'dot green'
   return statusDot;
 }
 
-const updateMemberDetails = (table, name,ticks) => {
-  let tr = createTrWithData('td',name,ticks);
+const getTallyMarks = (tallyList,ticks) => {
+  for (let i = 0; i < ticks; i++) appendChilds(tallyList,createElement('li'));
+}
+
+const updateTally = (tr,ticks) => {
+  const tallyList = createElement('ol');
+  getTallyMarks(tallyList,ticks);
+  const tallyData = createElement('td');
+  appendChilds(tallyData,tallyList);
+  appendChilds(tr, tallyData);
+}
+const updateStatus = (tr,ticks) => {
   const statusSpan = getStatus(ticks);
   const spanData = createElement('td');
-  appendChilds(spanData,statusSpan)
-  appendChilds(tr, spanData)
+  appendChilds(spanData,statusSpan);
+  appendChilds(tr, spanData);
+}
 
+const addTickButton = tr => {
+  const tickButton = createElement('button', 'Add', 'addTick');
+  const tickData =createElement('td');
+  appendChilds(tickData,tickButton);
+  appendChilds(tr,tickData);
+}
+
+const updateMemberDetails = (table, name,ticks) => {
+  let tr = createTrWithData('td',name);
+  updateTally(tr,ticks);
+  updateStatus(tr,ticks);
+  addTickButton(tr);
   appendChilds(table,tr);
+}
+
+const addTick = ({target}) => {
+  const memberName = target.parentNode.parentNode.firstChild.innerText;
+  const params = JSON.stringify({"memberName":memberName})
+  let xhttp = new XMLHttpRequest();
+  xhttp.open("PUT", "/tick", true);
+  xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhttp.send(params);
+  fetchMeterDetails();
 }
 
 const displayMemberDetails = function() {
@@ -39,18 +72,20 @@ const displayMemberDetails = function() {
   const shownDetails = getEle('table');
   const updatedDetails = createElement('table');
   const heading = createTrWithData(
-    'th', 'Name' , 'Number Of Ticks', 'Icecream Treat Status');
+    'th', 'Name' , 'Number Of Ticks', 'Treat Status', 'Add Tick');
   updatedDetails.appendChild(heading);
   Object.keys(response).forEach((key,index)=>{
       updateMemberDetails(updatedDetails, key, response[key]);
   })
   shownDetails.replaceWith(updatedDetails);
+  let addButtons = document.querySelectorAll('button[class=addTick]');
+  addButtons.forEach(button => button.onclick = addTick);
 }
 
-const getMeterDetails = () => {
-  var xhttp = new XMLHttpRequest();
+const fetchMeterDetails = () => {
+  let xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = displayMemberDetails;
   xhttp.open("GET", "/members", true);
   xhttp.send();
 }
-window.onload = getMeterDetails;
+window.onload = fetchMeterDetails;
